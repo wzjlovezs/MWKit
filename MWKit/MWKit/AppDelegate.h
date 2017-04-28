fe6078d3f96f1e101b284f0ffadf9ed346409ffa
MikeWang <wzjenjoyprogram@gmail.com>
2017-04-28 17:00:47 +0800

    init

---
+++ MWKit/MWKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MWKit
+//
+//  Created by MikeWang on 2017/4/25.
+//  Copyright © 2017年 MikeWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
